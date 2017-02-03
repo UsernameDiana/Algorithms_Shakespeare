4bfb262dff8ac8ee7bfb870b4bc303aac05c55d9
stinaanita <stina-stud@live.dk>
2017-02-03 12:07:17 +0100

    reading file

--- SortingArray/src/sortingarray/main.java
+++ SortingArray/src/sortingarray/main.java
@@ -1,25 +1,28 @@
 
 package sortingarray;
 
+import java.io.IOException;
+
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SortingArray sa = new SortingArray(2000);
 
-        sa.arrayPrinter(); 
-        int[] array = sa.getArray();
+//        sa.arrayPrinter(); 
+//        int[] array = sa.getArray();
+//        
+//        StopWatch timer = new StopWatch();
+//        //sa.selectionSort(array); //0.001
+//        sa.insertionSort(array); //0.017
+//        double time = timer.endTime();
+//        
+//        System.out.println("sorted");
+//        System.out.println("time: " + time);
+//        
+//        sa.arrayPrinter();
+
+        fileUtility fu = new fileUtility();
         
-        StopWatch timer = new StopWatch();
-        //sa.selectionSort(array); //0.001
-        sa.insertionSort(array); //0.017
-        double time = timer.endTime();
-        
-        System.out.println("sorted");
-        System.out.println("time: " + time);
-        
-        sa.arrayPrinter();
-       // fileUtility fu = new fileUtility();
-        
-       //String[] words = fileUtility.toStringArray("shakespeare-complete-works.txt", "[^A-Za-z]"); 
+       String[] words = fileUtility.toStringArray("shakespeare/shakespeare-complete-works.txt", "[^A-Za-z]"); 
         
     }
 }
