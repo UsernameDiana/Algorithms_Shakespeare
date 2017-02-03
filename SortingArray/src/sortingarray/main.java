36f43651f498a2981266df29200fe6bd7b59d743
jb <jb@jb>
2017-02-03 14:13:19 +0100

    ckanged insertion sort value from int to string

--- SortingArray/src/sortingarray/main.java
+++ SortingArray/src/sortingarray/main.java
@@ -1,25 +1,24 @@
-
 package sortingarray;
 
 import java.io.IOException;
 
 public class main {
+
     public static void main(String[] args) throws IOException {
         SortingArray sa = new SortingArray(2000);
 
 //        sa.arrayPrinter(); 
 //        int[] array = sa.getArray();
-//        
-//        StopWatch timer = new StopWatch();
-//        //sa.selectionSort(array); //0.001
+//        sa.selectionSort(array); //0.001
 //        sa.insertionSort(array); //0.017
-//        double time = timer.endTime();
-//        
 //        System.out.println("sorted");
 //        System.out.println("time: " + time);
-//        
 //        sa.arrayPrinter();
 
-       String[] words = fileUtility.toStringArray("build/classes/shakespeare/shak.txt", "[^A-Za-z]"); 
-        
-       sa.insertionSort(words); // we need to change int[] array to string [] in our sort methods
+        String[] words = fileUtility.toStringArray("build/classes/shakespeare/shak.txt", "[^A-Za-z]");
+
+        StopWatch timer = new StopWatch();
+        sa.insertionSort(words); // we need to change int[] array to string [] in our sort methods
+        double time = timer.endTime();
+    }
+}
