6156a429fb05e533d19019dde4ae668dd5d66969
stinaanita <stina-stud@live.dk>
2017-02-08 15:15:19 +0100

    merge sort abstract, and bottom up.

--- SortingArray/src/sortingarray/SortingArray.java
+++ SortingArray/src/sortingarray/SortingArray.java
@@ -7,6 +7,7 @@ import java.util.Random;
 public class SortingArray {
 
     public String[] array;
+    private static Comparable[] temp;
 
     // STEP ONE we need this to take in strings instead of int
     public SortingArray(int size) {
@@ -24,11 +25,56 @@ public class SortingArray {
         this.array = array;
     }
 
+//    public int less(Comparable[] a,Comparable[] b)
+//    {
+//        if () return a;
+//       
+//        return 0;
+//    }
     
-    public static void mergeSort(String[] array){
-        
+    
+    public static void mergeAbstract(Comparable[] a, int subIndex, int mid, int hi) { // Merge into new array
+        int i = subIndex, j = mid + 1;
+        for (int k = subIndex; k <= hi; k++) // Copy a[subIndex..hi] to aux[subIndex..hi].
+        {
+            temp[k] = a[k];
+        }
+        for (int k = subIndex; k <= hi; k++) // Merge back to a[subIndex..hi].
+        {
+            if (i > mid) 
+            {
+                a[k] = temp[j++];
+            } 
+            else if (j > hi) 
+            {
+                a[k] = temp[i++];
+            } 
+            else if (less(temp[j], temp[i])) 
+            {
+                a[k] = temp[j++];
+            } 
+            else 
+            {
+                a[k] = temp[i++];
+            }
+        }
+    }
+   
+    
+    public static void mergeSort(Comparable[] a) { // Do lg N passes of pairwise merges.
+        int N = a.length;
+        temp = new Comparable[N];
+        for (int subSize = 1; subSize < N; subSize = subSize + subSize) // subSize: subarray size
+        {
+            for (int subIndex = 0; subIndex < N - subSize; subIndex += subSize + subSize) // subIndex: subarray index
+            {
+                mergeAbstract(a, subIndex, subIndex + subSize - 1, Math.min(subIndex + subSize + subSize - 1, N - 1));
+            }
+        }
     }
 
+            
+   
     public String[] insertionSort(String[] array) { // comparing the neighbouring value
 
         int position;
@@ -50,7 +96,7 @@ public class SortingArray {
     }
 
     
-public String[] selectionSort(String[] array) {
+    public String[] selectionSort(String[] array) {
         String temp, minValue;
         int innerLoopIndex, minIndex = 0;
 
@@ -95,4 +141,19 @@ public String[] selectionSort(String[] array) {
             System.out.println("numbers : " + array[i]);
         }
     }
+    
+    public boolean isSorted(int limit) {
+        if (limit <= 0 || words.length < limit) {
+            limit = words.length;
+        }
+        String temp = words[0];
+        for (int i = 1; i < limit; i++) {
+            if (temp.compareTo(words[i]) > 0) {
+                return false;
+            }
+            temp = words[i];
+        }
+        return true;
+    }
+
 }
