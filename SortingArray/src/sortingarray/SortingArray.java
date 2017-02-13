85be24073c9ee014ade7e4fabc8f17dad4444f83
stinaanita <stina-stud@live.dk>
2017-02-13 19:38:49 +0100

    Fixed the compare method in Abstract in-place Merge sort, wrote more notes on the different types og merge sorting.

--- SortingArray/src/sortingarray/SortingArray.java
+++ SortingArray/src/sortingarray/SortingArray.java
@@ -7,7 +7,7 @@ import java.util.Random;
 public class SortingArray {
 
     public String[] array;
-    private static Comparable[] temp;
+    private static Comparable[] temp; // auxiliary(additional) array for merges in Top-down merge sort
 
     public SortingArray(int size) {
         array = new String[size]; // created new list of array with given size
@@ -31,9 +31,11 @@ public class SortingArray {
 //        return 0;
 //    }
     
+    // Abstract in-place merge trace
+    //copy everything to an auxiliary array and then merging back to the original.
     public static void merge(Comparable[] pivot, int subIndex, int mid, int hi) { // Merge into new array
         int i = subIndex, j = mid + 1;
-        for (int k = subIndex; k <= hi; k++) // Copy pivot[subIndex..hi] to temp[subIndex..hi].
+        for (int k = subIndex; k <= hi; k++) // Copy pivot[subIndex..hi] to temp[subIndex..hi]
         {
             temp[k] = pivot[k];
         }
@@ -45,8 +47,8 @@ public class SortingArray {
             } 
             else if (j > hi) {
                 pivot[k] = temp[i++];
-            } 
-            else if (less(temp[j], temp[i])) 
+            }                                   // if ( x[left].compareTo(x[right]) <= 0 
+            else if (temp[j].compareTo(temp[i]) <= 0 ) //position > 0 && array[position - 1].compareTo(valueToInsert) > 0 //less(temp[j], temp[i])
             {
                 pivot[k] = temp[j++];
             } 
@@ -56,6 +58,12 @@ public class SortingArray {
             }
         }
     }
+//    This method merges by fi rst copying into the auxiliary array aux[] 
+//    then merging back to a[]. In the merge (the second for loop), 
+//    there are four conditions: left half exhausted (take from the right), 
+//    right half exhausted (take from the left), 
+//    current key on right less than current key on left (take from the right), 
+//    and current key on right greater than or equal to current key on left (take from the left).
 
     public static void mergeBottomsUp(Comparable[] a) { // Do lg N passes of pairwise merges.
         int N = a.length;
@@ -68,14 +76,14 @@ public class SortingArray {
             }
         }
     }
-
-    
+ 
     
     public static void mergeTopDown(Comparable[] a) {
         temp = new Comparable[a.length]; // Allocate space just once.
         mergeTopDown(a, 0, a.length - 1);
     }
-
+    
+   // Recursive mergesort implementation based on this abstract inplace merge.
     private static void mergeTopDown(Comparable[] a, int lo, int hi) { // Sort a[lo..hi].
         if (hi <= lo) {
             return;
