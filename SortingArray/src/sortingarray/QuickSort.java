9aaec651aadf8386f366164a3f0bbf7ace3e761e
stinaanita <stina-stud@live.dk>
2017-02-16 19:24:56 +0100

    Got the quick sort working

---
+++ SortingArray/src/sortingarray/QuickSort.java
@@ -0,0 +1,63 @@
+package sortingarray;
+
+
+/**
+ *
+ * @author Kristin
+ */
+//    Quicksort is temp divide -and -conquer method for sorting.It
+//    works by partitioning an array into two subarrays, then sorting the subarrays independently.
+
+public class QuickSort {
+    
+    private static Comparable[] temp; 
+
+    public QuickSort(Comparable[] array)
+    {
+        this.temp = array;
+    }
+
+    public static void quickSort(Comparable[] temp) { // throws IllegalArgumentException
+       // StdRandom.shuffle(temp); // Eliminate dependence on input.
+        sort(temp, 0, temp.length - 1);
+    }
+
+    private static void sort(Comparable[] temp, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+        int j = partition(temp, lo, hi); // Partition (see page 291).
+        sort(temp, lo, j - 1); // Sort left part temp[lo .. j-1].
+        sort(temp, j + 1, hi); // Sort right part temp[j+1 .. hi].
+    }
+
+    private static int partition(Comparable[] temp, int lo, int hi) { // Partition into temp[lo..i-1], temp[i], temp[i+1..hi].
+        int i = lo, j = hi + 1; // left and right scan indices
+        Comparable v = temp[lo]; // partitioning item
+        while (true) { // Scan right, scan left, check for scan complete, and exchange.
+            while (temp[++i].compareTo(v) < 0) { // temp[j].compareTo(temp[i]) <= 0
+                if (i == hi) {
+                    break;
+                }
+            }
+            while (v.compareTo( temp[--j]) < 0) {
+                if (j == lo) {
+                    break;
+                }
+            }
+            if (i >= j) {
+                break;
+            }
+            exch(temp, i, j);// 
+        }
+        exch(temp, lo, j); // Put v = temp[j] into position
+        return j; // with temp[lo..j-1] <= temp[j] <= temp[j+1..hi].
+    }
+
+     private static void exch(Object[] a, int i, int j) {
+            Object tmp = a[i];
+            a[i] = a[j];
+            a[j] = tmp;
+        }
+    
+}
--- SortingArray/src/sortingarray/main.java
+++ SortingArray/src/sortingarray/main.java
@@ -31,14 +31,22 @@ public class main {
         // sa2.arrayPrinter();
 
         Comparable[] wordsForMerge = fileUtility.toStringArray("build/classes/shakespeare/shak.txt", "[^A-Za-z]");
+        
         MergeSort mSort = new MergeSort(wordsForMerge);
         StopWatch start3 = new StopWatch(); 
         mSort.mergeTopDown(wordsForMerge);
         double stop3 = start3.endTime();
         
+        Comparable[] wordsForQuick = fileUtility.toStringArray("build/classes/shakespeare/shak.txt", "[^A-Za-z]"); 
+        QuickSort qSort = new QuickSort(wordsForQuick);
+        StopWatch start4 = new StopWatch();
+        qSort.quickSort(wordsForQuick);
+        double stop4 = start4.endTime();
+        
         System.out.println("Time of Selection Sort: " + stop1);
         System.out.println("Time of Insertion Sort: " + stop2);
         System.out.println("Time of Merge Sort: " + stop3);
+        System.out.println("Time of Quick Sort: " + stop4);
     }
 
 }
