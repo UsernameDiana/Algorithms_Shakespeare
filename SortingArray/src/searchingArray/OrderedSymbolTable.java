5986785bb28a171adbe6b3791d15f21bb09c9586
stinaanita <stina-stud@live.dk>
2017-02-17 11:17:05 +0100

    notes in interfaces

--- SortingArray/src/searchingArray/OrderedSymbolTable.java
+++ SortingArray/src/searchingArray/OrderedSymbolTable.java
@@ -7,19 +7,19 @@ public interface OrderedSymbolTable< K extends Comparable<K>, V> extends SymbolT
 
     K max();
 
-    K floor(K key);
+    K floor(K key); // largest key less than or equal to key
 
-    K ceiling(K key);
+    K ceiling(K key); // smallest key greater than or equal to key
 
-    int rank(K key);
+    int rank(K key); // number of keys less than key
 
-    K select(int rank);
+    K select(int rank); // key of rank k
 
     void deleteMin();
 
     void deleteMax();
 
-    int size(K low, K high);
+    int size(K low, K high); // number of keys in [lo..hi]
 
     Iterable<K> keys(K low, K high);
 
