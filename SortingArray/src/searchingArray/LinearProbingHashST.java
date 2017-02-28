5e48ccf6e94c016aed8139deee2c540fb20b3072
stinaanita <stina-stud@live.dk>
2017-02-28 16:49:48 +0100

    Made an extra constructor for the rezising method.

--- SortingArray/src/searchingArray/LinearProbingHashST.java
+++ SortingArray/src/searchingArray/LinearProbingHashST.java
@@ -3,25 +3,30 @@ package searchingArray;
 public class LinearProbingHashST<Key, Value> {
 
     private int N; // number of key-value pairs in the table
-    private int M = 16; // size of linear-probing table
+    private int M = 16; // SIZE -- of linear-probing table
     private Key[] keys; // the keys
     private Value[] vals; // the values
 
     public LinearProbingHashST() {
-        this.M = 16;
         keys = (Key[]) new Object[M];
         vals = (Value[]) new Object[M];
-    }
+    } 
 
+    private LinearProbingHashST(int cap) {
+        this.M = cap;                       // I made a constructor that sets the value of M (the initial size)
+        keys = (Key[]) new Object[M];       // to the incomming value = cap, cap comes from the rezise method (M * 2)
+        vals = (Value[]) new Object[M];
+    }
+   
 //  Hash function - Compound keys. If the key type has multiple integer fields, we can typically mix them
 //  together in the way just described for String values.
     private int hash(Key key) {
         return (key.hashCode() & 0x7fffffff) % M;
     }
 
-    private void resize(int M) {
+    private void resize(int cap) {
         LinearProbingHashST<Key, Value> t;
-        t = new LinearProbingHashST<Key, Value>(M);
+        t = new LinearProbingHashST<Key, Value>(cap);
         for (int i = 0; i < M; i++) {
             if (keys[i] != null) {
                 t.put(keys[i], vals[i]);
