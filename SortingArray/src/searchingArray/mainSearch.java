d10de8db7f4b2eaf0e77affe0420177007aa5566
stinaanita <stina-stud@live.dk>
2017-03-06 10:43:59 +0100

    Wrapping my brain around the symbol tables...

--- SortingArray/src/searchingArray/mainSearch.java
+++ SortingArray/src/searchingArray/mainSearch.java
@@ -2,9 +2,9 @@
 package searchingArray;
 
 public class mainSearch {
-    public static void main(String[] args) {
-        
-        FlexibleArray<Integer> list = new FlexibleArray<>();
+    public static void main(String[] args) 
+    {
+        FlexibleArray<Word> arrayST = new FlexibleArray<>();
         // ArrayList<Integer> list = new ArrayList();
         
         LinearProbingHashST<String, Integer> lpHash = new LinearProbingHashST<>();
@@ -12,15 +12,23 @@ public class mainSearch {
         SequentialSearchST<String, Integer> ss = new SequentialSearchST<>();
 
         long t0 = System.currentTimeMillis();
-        for (int i = 0; i < 1_000_000; i++) {
-            list.add(i);
-        }
+//        for (Sting = 0; i < 1_000_000; i++) {
+//            arrayST.add(i);
+//        }
         long t1 = System.currentTimeMillis();
-
-        System.out.println("Time: " + ((t1 - t0) / 1000.0));
-        System.out.println("Value at index 123456: " + list.get(123456));
         
-//        
+        System.out.println("Time Milliseconds: " + ((t1 - t0) / 1000.0));
+
+//        long t2 = System.nanoTime();
+//        arrayST.get(123456);
+//        long t3 = System.nanoTime();
+//       
+//        System.out.println("ArraySymbolTable Value at index 123456: "+ ((t3-t2)/1000.0) + " Nanoseconds");
+//       
+
+
+
+////        
 //        ST<String, Integer> st;
 //        st = new ST<String, Integer>();
 //        
